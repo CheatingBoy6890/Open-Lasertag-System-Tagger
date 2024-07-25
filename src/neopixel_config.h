27386c28f718c968c6598e3ffd2dc6a550f9eed9
CheatingBoy6890 <119727726+CheatingBoy6890@users.noreply.github.com>
2024-07-25 09:12:05 +0200

    Update neopixel_config.h
    License for all src files

--- src/neopixel_config.h
+++ src/neopixel_config.h
@@ -1,4 +1,11 @@
+/*
+Copyright (c) <2024>, <Silas Hille>
+All rights reserved.
 
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. 
+
+*/
 
 // definition for all LEDs on the neopixel strip
 
